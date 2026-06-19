0286ea7eb01b47c4a925d1c8127b99e3ad71b624
h4r15h <harish-pushan>
2026-06-19 23:34:49 +0530

    : sanity check

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 
+source ./install_scripts/aws/docker_install.sh
+source ./install_scripts/aws/k3s_install.sh
+source ./install_scripts/aws/helpers/check_root.sh
+source ./install_scripts/aws/helpers/greater.sh
+
+
+check_root
+greeting_echo
+apt_install_docker
+check_version_kubernetes_install
 
 
 
